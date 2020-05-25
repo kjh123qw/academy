9a969869910ff41fa10472480456bfb686e9edfd
jeongho kim <kjh123qwa@naver.com>
2020-05-26 05:10:48 +0900

    'cm'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Grid, Header, Input, List, Segment } from "semantic-ui-react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Amplify, { API, graphqlOperation } from "aws-amplify";
-import { Connect, withAuthenticator } from "aws-amplify-react";
+// import { Grid, Header, Input, List, Segment } from "semantic-ui-react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+import Amplify, { graphqlOperation } from "aws-amplify";
+import { Connect } from "aws-amplify-react";
 import { getAcademyDb } from "./graphql/queries";
 
 import aws_exports from "./aws-exports";
@@ -25,13 +25,5 @@ const App = () => {
     </Connect>
   );
 };
-
-// const WithProvider = () => (
-//   <ApolloProvider client={client}>
-//     <Rehydrated>
-//       <App />
-//     </Rehydrated>
-//   </ApolloProvider>
-// );
 
 export default App;
