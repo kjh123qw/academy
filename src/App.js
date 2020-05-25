0014100b1ce2e9c7af96365e38f0a1d709776bc0
jeongho kim <kjh123qwa@naver.com>
2020-05-25 22:07:55 +0900

    'commit'

--- src/App.js
+++ src/App.js
@@ -1,60 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import appSyncConfig from "./aws-exports";
-import { ApolloProvider } from "react-apollo";
-import AWSAppSyncClient, { defaultDataIdFromObject } from "aws-appsync";
-import { Rehydrated } from "aws-appsync-react";
-
 import "./App.css";
-import Testhome from "./Components/Testhome";
 
-const Home = () => (
-  <div className="ui container">
-    <Testhome Id="3" />
-  </div>
-);
-
-const App = () => (
-  <Router>
-    <div>
-      <Route exact={true} path="/" component={Home} />
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  </Router>
-);
+  );
+}
 
-const client = new AWSAppSyncClient({
-  url: appSyncConfig.aws_appsync_graphqlEndpoint,
-  region: appSyncConfig.aws_appsync_region,
-  auth: {
-    type: appSyncConfig.aws_appsync_authenticationType,
-    apiKey: appSyncConfig.aws_appsync_apiKey,
-  },
-  cacheOptions: {
-    dataIdFromObject: (obj) => {
-      let id = defaultDataIdFromObject(obj);
-
-      if (!id) {
-        const { __typename: typename } = obj;
-        switch (typename) {
-          case "Comment":
-            return `${typename}:${obj.commentId}`;
-          default:
-            return id;
-        }
-      }
-
-      return id;
-    },
-  },
-});
-
-const WithProvider = () => (
-  <ApolloProvider client={client}>
-    <Rehydrated>
-      <App />
-    </Rehydrated>
-  </ApolloProvider>
-);
-
-export default WithProvider;
+export default App;
--- src/Components/Testhome.js
+++ src/Components/Testhome.js
@@ -1,27 +1,27 @@
-import React from "react";
-import { graphql } from "react-apollo";
-import { getAcademyDb } from "../graphql/queries";
-import gql from "graphql-tag";
+// import React from "react";
+// import { graphql } from "react-apollo";
+// import { getAcademyDb } from "../graphql/queries";
+// import gql from "graphql-tag";
 
-class Testhome extends React.Component {
-  render() {
-    const { Id, Subject } = this.props;
-    console.log({ Id, Subject });
-    return <div> Hello</div>;
-  }
-}
+// class Testhome extends React.Component {
+//   render() {
+//     const { Id, Subject } = this.props;
+//     console.log({ Id, Subject });
+//     return <div> Hello</div>;
+//   }
+// }
 
-export default graphql(gql(getAcademyDb), {
-  options: ({ Id: id }) => ({
-    fetchPolicy: "cache-first",
-    variables: { Id: id },
-  }),
-  props: ({
-    data: {
-      getAcademyDB: { Id, Subject },
-    },
-  }) => ({
-    Id,
-    Subject,
-  }),
-})(Testhome);
+// export default graphql(gql(getAcademyDb), {
+//   options: ({ Id: id }) => ({
+//     fetchPolicy: "cache-first",
+//     variables: { Id: id },
+//   }),
+//   props: ({
+//     data: {
+//       getAcademyDB: { Id, Subject },
+//     },
+//   }) => ({
+//     Id,
+//     Subject,
+//   }),
+// })(Testhome);
