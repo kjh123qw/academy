203998c92a20f1fca2e2853ed8a61159ca89f096
jeongho kim <kjh123qwa@naver.com>
2020-05-26 11:42:33 +0900

    'cm'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
 // import { Grid, Header, Input, List, Segment } from "semantic-ui-react";
 // import { BrowserRouter as Router, Route } from "react-router-dom";
+import Amplify, { API, graphqlOperation } from "aws-amplify";
+import { Connect, withAuthenticator } from "aws-amplify-react";
+import { getAcademyDb } from "./graphql/queries";
+
+import aws_exports from "./aws-exports";
+Amplify.configure(aws_exports);
 
 function App() {
-  return <div>Hello</div>;
-}
+  return (
+    <Connect query={graphqlOperation(getAcademyDb, { Id: "3" })}>
+      {({ data, loading }) => {
+        if (loading) {
+          return <div>Loading...</div>;
+        }
 
+        return (
+          <div>
+            Hello <b>{data.getAcademyDB.Subject}</b>
+          </div>
+        );
+      }}
+    </Connect>
+  );
+}
 export default App;
