71735eb8e5a47a1effaa3f8e29ebcbbc6c8657b7
jeongho kim <kjh123qwa@naver.com>
2020-05-26 12:29:29 +0900

    'cm'

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,2 +1,51 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
+
+export const createMyType = /* GraphQL */ `
+  mutation CreateMyType(
+    $input: CreateMyTypeInput!
+    $condition: ModelMyTypeConditionInput
+  ) {
+    createMyType(input: $input, condition: $condition) {
+      id
+      title
+      content
+      price
+      rating
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateMyType = /* GraphQL */ `
+  mutation UpdateMyType(
+    $input: UpdateMyTypeInput!
+    $condition: ModelMyTypeConditionInput
+  ) {
+    updateMyType(input: $input, condition: $condition) {
+      id
+      title
+      content
+      price
+      rating
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteMyType = /* GraphQL */ `
+  mutation DeleteMyType(
+    $input: DeleteMyTypeInput!
+    $condition: ModelMyTypeConditionInput
+  ) {
+    deleteMyType(input: $input, condition: $condition) {
+      id
+      title
+      content
+      price
+      rating
+      createdAt
+      updatedAt
+    }
+  }
+`;
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,11 +1,36 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const getAcademyDb = /* GraphQL */ `
-  query GetAcademyDb($Id: String!) {
-    getAcademyDB(Id: $Id) {
-      Id
-      Subject
+export const getMyType = /* GraphQL */ `
+  query GetMyType($id: ID!) {
+    getMyType(id: $id) {
+      id
+      title
+      content
+      price
+      rating
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listMyTypes = /* GraphQL */ `
+  query ListMyTypes(
+    $filter: ModelMyTypeFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listMyTypes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        title
+        content
+        price
+        rating
+        createdAt
+        updatedAt
+      }
+      nextToken
     }
   }
 `;
